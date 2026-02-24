d36a7074e89bb638e18607cd418262f3cf1347b9
Jack Bigler <jbigs6134@gmail.com>
2026-02-24 16:15:00 -0600

    Created a todo class in order to have datetime sorting

--- lib/main.dart
+++ lib/main.dart
@@ -13,21 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo App Tutorial',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'To-Do List Home Page'),
@@ -56,17 +41,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           "Todo Application",
         ),
+      ),
+
+      body: ListView(
+        children: [
+          for (var item in toDoList)
+
+
+          
+        ],
       ),
 
       backgroundColor: Colors.indigo,
     );
   
   }
+}
+
+
+class TodoItem{
+  final String title;
+  final DateTime dueAt;
+
+  TodoItem({required this.title, required this.dueAt});
 }
